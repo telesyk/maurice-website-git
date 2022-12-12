09a07c7fc32302aa2a317a860474be41b8f0c9cf
telesyk <telesyk89@gmail.com>
2022-12-12 13:35:50 +0200

    Update HTML structure of the header & navbar blocks
    Update style naming
    Add styling for mobile & tablets menu
    Add JS for mobile menu triggering

---
+++ script/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const navbarElement = document.querySelector('.navbar');
+const mobileMenuButton = document.getElementById('hamburger-menu');
+
+mobileMenuButton.addEventListener('click', handleMobileMenu);
+
+function handleMobileMenu(event) {
+  event.preventDefault();
+  console.log(event);
+  
+  navbarElement.classList.toggle('is-active');
+}
