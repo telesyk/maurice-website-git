5dbc556f075607d7c5e490370e90c1c6b9747036
telesyk <telesyk89@gmail.com>
2022-12-14 12:13:33 +0200

    start search

--- script/index.js
+++ script/index.js
@@ -2,12 +2,14 @@ import { SELECTOR } from "./constants";
 import mobileMenu from "./module-mobile-menu";
 import formValidation from "./module-form-validation";
 
-/* Mobile Navigation */
-if (document.getElementById(SELECTOR.mobileMenu)) {
-  mobileMenu();
-}
-
-/* Form validation */
-if (document.getElementById(SELECTOR.contactForm)) {
-  formValidation();
-}
+window.onload = () => {
+  /* Mobile Navigation */
+  if (document.getElementById(SELECTOR.mobileMenu)) {
+    mobileMenu();
+  }
+  
+  /* Form validation */
+  if (document.getElementById(SELECTOR.contactForm)) {
+    formValidation();
+  }
+};
